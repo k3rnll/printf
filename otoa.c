0808b049b021a38fb97e494a5b745a255a7a4e52
spider624 <spider624@mail.ru>
2020-08-03 21:47:58 +0300

    otoa fixed

--- otoa.c
+++ otoa.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   otoa.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clouise <clouise@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/03 21:30:02 by clouise           #+#    #+#             */
+/*   Updated: 2020/08/03 21:44:17 by clouise          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-static int	ft_olen(unsigned long long int u)
+static int		ft_olen(unsigned long long int u)
 {
-	int		len;
+	int			len;
 
 	len = 0;
 	while (u / 8 != 0)
@@ -14,12 +26,12 @@ static int	ft_olen(unsigned long long int u)
 	return (len);
 }
 
-char 		*convert_o(char *str, t_pfdata *pfdata)
+char			*convert_o(char *str, t_pfdata *pfdata)
 {
-	char 	*tmp;
-	char 	*res;
-	int 	len;
-	int 	i;
+	char		*tmp;
+	char		*res;
+	int			len;
+	int			i;
 
 	i = 0;
 	len = ft_strlen(str);
@@ -33,29 +45,27 @@ char 		*convert_o(char *str, t_pfdata *pfdata)
 		free(tmp);
 		return (res);
 	}
-	if (pfdata->dot && !pfdata->dotprec && !pfdata->alt && str[0] == '0') {
+	if (pfdata->dot && !pfdata->dotprec && !pfdata->alt && str[0] == '0')
+	{
 		pfdata->zero = 0;
 		str[0] = 0;
 	}
 	return (str);
 }
 
-char 	*otoa(unsigned long long int n, t_pfdata *pfdata)
+char			*otoa(unsigned long long int n, t_pfdata *pfdata)
 {
 	int			len;
 	char		*str;
 
 	if (pfdata->mod[0] == 'h' && n > 0)
-		n = pfdata->mod[1] == 'h' ? n & 0x00000000000000FF : n & 0x000000000000FFFF;
+		n = pfdata->mod[1] == 'h' ? n & 0x00000000000000FF : n
+					& 0x000000000000FFFF;
 	else if (pfdata->mod[0] != 'l')
 		n = n & 0x00000000FFFFFFFF;
-//	else
-//		n = n & 0x00000000FFFFFFFF;
 	len = ft_olen(n);
 	if (!(str = ft_strnew(len + 1)))
 		return (NULL);
-
-
 	while (n / 8 != 0)
 	{
 		str[len - 1] = ((n % 8) + '0');
@@ -65,5 +75,5 @@ char 	*otoa(unsigned long long int n, t_pfdata *pfdata)
 	str[len - 1] = ((n % 8) + '0');
 	if (pfdata->alt && str[0] != '0')
 		str = ft_strjoin("0", str);
-	return(convert_o(str, pfdata));
+	return (convert_o(str, pfdata));
 }
