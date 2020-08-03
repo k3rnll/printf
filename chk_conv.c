a00e3f26b7551b0cc3f2b3dfc3de81b8c3a26e38
k3rnll <kanaikin.greg@gmail.com>
2020-08-03 22:37:26 +0300

    norm madness

--- chk_conv.c
+++ chk_conv.c
@@ -1,14 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chk_conv.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmarkita <tmarkita@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/03 22:33:42 by k3                #+#    #+#             */
+/*   Updated: 2020/08/03 22:35:29 by k3               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int 	chk_conv(t_pfdata *pfdata)
+int		chk_conv(t_pfdata *pfdata)
 {
-	int 	k;
+	int		k;
 
 	while (pfdata->format[pfdata->i])
 	{
 		k = 0;
-		while (pfdata->conversion[k] != '\0') {
-			if (pfdata->format[pfdata->i] == pfdata->conversion[k]) {
+		while (pfdata->conversion[k] != '\0')
+		{
+			if (pfdata->format[pfdata->i] == pfdata->conversion[k])
+			{
 				chk_prec(pfdata);
 				reparse(pfdata);
 				digits(pfdata);
@@ -17,9 +31,8 @@ int 	chk_conv(t_pfdata *pfdata)
 			k++;
 		}
 		if (pfdata->format[pfdata->i] == '\0')
-			break;
-//		ft_putchar(pfdata->format[pfdata->i]);
+			break ;
 		pfdata->i++;
 	}
-	return(0);
+	return (0);
 }
