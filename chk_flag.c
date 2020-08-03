4973b03dc008ceb50cbb994a0d33d64397680fec
k3rnll <kanaikin.greg@gmail.com>
2020-08-03 22:45:48 +0300

    norm madness chk_flag

--- chk_flag.c
+++ chk_flag.c
@@ -1,16 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chk_flag.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmarkita <tmarkita@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/03 22:40:15 by k3                #+#    #+#             */
+/*   Updated: 2020/08/03 22:42:44 by k3               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-void 	chk_flag(t_pfdata *pfdata)
+void	chk_flag(t_pfdata *pfdata)
 {
-	while (ft_strchr(pfdata->flags, pfdata->format[pfdata->i]))
+	const char	*str;
+
+	str = pfdata->format;
+	while (ft_strchr(pfdata->flags, str[pfdata->i]))
 	{
-		pfdata->alt = (pfdata->format[pfdata->i] == '#') ? '#' : pfdata->alt;
-		pfdata->align = (pfdata->format[pfdata->i] == '-') ? '-' : pfdata->align;
-		pfdata->zero = (pfdata->format[pfdata->i] == '0') ? 1 : pfdata->zero;
-		pfdata->space = (pfdata->format[pfdata->i] == ' ') ? ' ' : pfdata->space;
-		pfdata->plus = (pfdata->format[pfdata->i] == '+') ? '+' : pfdata->plus;
-		pfdata->dot = (pfdata->format[pfdata->i] == '.') ? '.' : pfdata->dot;
-//		pfdata->prec = (pfdata->format[pfdata->i] == '*') ? va_arg(pfdata->args, int) : 0;
+		pfdata->alt = (str[pfdata->i] == '#') ? '#' : pfdata->alt;
+		pfdata->align = (str[pfdata->i] == '-') ? '-' : pfdata->align;
+		pfdata->zero = (str[pfdata->i] == '0') ? 1 : pfdata->zero;
+		pfdata->space = (str[pfdata->i] == ' ') ? ' ' : pfdata->space;
+		pfdata->plus = (str[pfdata->i] == '+') ? '+' : pfdata->plus;
+		pfdata->dot = (str[pfdata->i] == '.') ? '.' : pfdata->dot;
 		pfdata->i++;
 	}
 }
